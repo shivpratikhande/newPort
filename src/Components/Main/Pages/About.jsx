2c729dbbc8ddebf4e897c2d3ba1eb1d86b4c4d08
shivpratikhande <shivpratikhande2017@gmail.com>
2024-04-27 14:44:46 +0530

    bug

--- src/Components/Main/Pages/About.jsx
+++ src/Components/Main/Pages/About.jsx
@@ -3,7 +3,7 @@ import "./Css/About.css"
 
 function About() {
   return (
-    <div className=' inline-block p-10 border border-black border-2 rounded-3xl '>
+    <div className=' inline-block p-10 border-black border-2 rounded-3xl '>
       <h2 className=' text-3xl font-bold text-center pb-5 mb-2  '> <span className=' py-2 px-5 rounded-xl hover:bg-black hover:text-white'>About Me</span></h2>
       <h3 className=' text-lg font-sans flex  '> "Hello, my name is Shivpratik Hande, and I am currently an undergraduate student pursuing a Bachelor's degree in Information Technology. <br /> I have always been deeply passionate about technology and am particularly fascinated by its diverse applications. My academic journey thus far has equipped me with a strong foundation in programming, database management, system analysis, and web development.
       <br /> Through hands-on projects and coursework, I have honed my skills in various programming languages, database systems, and web development frameworks. I am driven by a desire to contribute meaningfully to the field of information technology 

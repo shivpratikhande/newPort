49a9e4a82fbb5274a46f122714f788a350970a58
shivpratikhande <shivpratikhande2017@gmail.com>
2024-04-27 14:27:11 +0530

    added some changes in pages

--- frontend/src/Components/Main/Pages/About.jsx
+++ frontend/src/Components/Main/Pages/About.jsx
@@ -3,10 +3,11 @@ import "./Css/About.css"
 
 function About() {
   return (
-    <div className='about-content'>
-      <h2>About me</h2>
-      <h3>Hii, this is me Shivpratik Hande. <br /> I am currently an undergraduate student pursuing a Bachelor's degree in Information Technology. I am enthusiastic about exploring various facets of technology and acquiring knowledge in areas such as programming, database management, and system analysis. My academic journey aims to equip me with the skills and expertise necessary 
-        to contribute meaningfully to the ever-evolving field of information technology.</h3>
+    <div className=' inline-block p-10 border border-black border-2 rounded-3xl '>
+      <h2 className=' text-3xl font-bold text-center pb-5 mb-2  '> <span className=' py-2 px-5 rounded-xl hover:bg-black hover:text-white'>About Me</span></h2>
+      <h3 className=' text-lg font-sans flex  '> "Hello, my name is Shivpratik Hande, and I am currently an undergraduate student pursuing a Bachelor's degree in Information Technology. <br /> I have always been deeply passionate about technology and am particularly fascinated by its diverse applications. My academic journey thus far has equipped me with a strong foundation in programming, database management, system analysis, and web development.
+      <br /> Through hands-on projects and coursework, I have honed my skills in various programming languages, database systems, and web development frameworks. I am driven by a desire to contribute meaningfully to the field of information technology 
+      and am excited about the possibilities it offers. <br />With a blend of technical expertise and a proactive approach to learning, I am eager to embark on new challenges and opportunities in this dynamic field."</h3>
         <br />
         <h3></h3>
     </div>
--- frontend/src/Components/Main/Pages/Blog.jsx
+++ frontend/src/Components/Main/Pages/Blog.jsx
@@ -3,21 +3,21 @@ import "./Css/Blog.css"
 
 function Blog() {
   return (
-    <div className='blog'>
-      <h2>Blogs</h2>
-      <ul>
-        <li><p>I do write some blogs regarding the trending techstack</p></li>
-        <li> <p>In my blog contributions, I aim to share valuable knowledge with the community, offering insights, tutorials, and practical advice to assist fellow developers and enthusiasts. I believe in creating a collaborative environment where shared experiences can empower others in navigating the ever-evolving tech landscape. My goal is to simplify complex concepts, address common challenges,
-         and explore emerging trends, fostering a professional and inclusive community. </p></li>
-         <li><p>By sharing my expertise, I hope to inspire and contribute to a thriving network of individuals
-           dedicated to continuous learning and innovation in the field of technology. </p></li>   
-      </ul>
-      <button>Click Here</button> to visit my Blogs
+    <div className=' inline-block p-10 border-black border-2 rounded-3xl '>
+      <div className=' '>
+      <h2 className=' text-3xl font-bold text-center pb-5 mb-2  '> <span className=' py-2 px-5 rounded-xl hover:bg-black hover:text-white'>Blogs</span></h2>
+      <h3 className=' text-lg font-sans   '>
+        <p className=''>I do write some blogs regarding the trending techstack in the internet World.</p>
+         <p>In my blog contributions, I aim to share valuable knowledge with the community, offering insights, tutorials, and practical advice to assist fellow developers and enthusiasts. I believe in creating a collaborative environment where shared experiences can empower others in navigating the ever-evolving tech landscape. My goal is to simplify complex concepts, address common challenges,
+          and explore emerging trends, fostering a professional and inclusive community. </p>
+        <p>By sharing my expertise, I hope to inspire and contribute to a thriving network of individuals
+          dedicated to continuous learning and innovation in the field of technology. </p>     </h3>
+      </div>
       
-      
-         
- </div>
+    </div>
+
   )
 }
 
 export default Blog
+
